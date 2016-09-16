32b97a8d871e733b4bf46151a86726c6267d1a75
Petja <hello@petja.me>
2016-09-16 12:45:14 +0000

    It's working!

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,7 +1,45 @@
 public class NumberGuess {
     public static void main(String[] args) {
-        boolean answer = askQuestion("Is the number divisible with two?");
-        System.out.println(answer);
+        int minimum = 1;
+        int maximum = 1000;
+
+        // boolean answer = askQuestion("Is the number divisible with two?");
+        // System.out.println(answer);
+
+        // Intro
+        System.out.println("\nPlease think any number between " + minimum + " and " + maximum);
+        System.console().readLine("Press [ENTER] when you're ready to go!\n");
+
+        // Keep count on rounds
+        int round = 0;
+
+        while(minimum != maximum) {
+            round++;
+
+            int[] range = askNumber(round, minimum, maximum);
+            minimum = range[0];
+            maximum = range[1];
+        }
+
+        System.out.println("Number solved! It was: " + minimum);
+    }
+
+    public static int[] askNumber(int round, int minimum, int maximum) {
+        int number = (minimum + maximum) / 2;
+
+        int[] output = new int[2];
+        output[0] = minimum;
+        output[1] = maximum;
+
+        boolean answer = askQuestion("\nRound " + round + ": Is the number greater than " + number + "?");
+
+        if (answer) {
+            output[0] = number + 1;
+        } else {
+            output[1] = number;
+        }
+        
+        return output;
     }
 
     public static boolean askQuestion(String question) {
